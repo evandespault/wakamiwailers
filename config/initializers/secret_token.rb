ff608628b22844654a0ad86c0be5942ff5ec1763
Evan Despault <evan11@gmail.com>
2012-05-21 21:54:13 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wakamiwailers::Application.config.secret_token = '0968ef155bc44ddf4900e0240ae3e0d6e76115869f25a4d15bd87348c2a7e27eaa024b6c3c7b130cc550500d66881863b89c27d4358d497b718a4b98351fc9e7'
