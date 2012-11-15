756bd68b12d048134749ad506e008cb16e2d215b
Evan Despault <evan11@gmail.com>
2012-11-14 16:39:00 -0800

    fix js

--- public/javascripts/javascripts.js
+++ public/javascripts/javascripts.js
@@ -1,9 +1,7 @@
-<script type="text/javascript"><![CDATA[
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=260866670620790";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-]]></script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=260866670620790";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
