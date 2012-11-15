38c2398c00f2ee8e08e150a46e5347e185d9df45
Evan Despault <evan11@gmail.com>
2012-11-14 16:32:47 -0800

    fix js

--- public/javascripts/javascripts.js
+++ public/javascripts/javascripts.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+<script type="text/javascript"><![CDATA[
 	(function(d, s, id) {
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
@@ -6,4 +6,4 @@
 	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=260866670620790";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
-</script>
+]]></script>
